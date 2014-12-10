9bd001a86d15c86e6ec00d1866315fcf065481b9
eboissier <eboissier@e07a6ad9-7c28-0410-a8f0-b6e01509c4f6>
2014-12-10 13:40:57 +0000

    change output of services + up version
    git-svn-id: https://gateway.terradue.com/repo/products/sugar/Terradue.WebService.Model/branches/develop@40139 e07a6ad9-7c28-0410-a8f0-b6e01509c4f6

---
+++ Terradue.WebService.Model/Terradue/WebService/Model/Global.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ServiceStack.ServiceHost;
+using System.Collections.Generic;
+
+namespace Terradue.WebService.Model {
+
+    //---------------------------------------------------------------------------------------------------------------------
+
+    [Route("/config", "GET", Summary = "GET main config", Notes = "Get main config")]
+    public class GetConfig : IReturn<List<KeyValuePair<string, string>>> {}
+
+
+    //---------------------------------------------------------------------------------------------------------------------
+
+    public class WebKeyValue {
+        [ApiMember(Name="Key", Description = "Key", ParameterType = "query", DataType = "String", IsRequired = false)]
+        public string Key { get; set; }
+        [ApiMember(Name="Value", Description = "Value", ParameterType = "query", DataType = "String", IsRequired = false)]
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Terradue.WebService.Model.WebNameValue"/> class.
+        /// </summary>
+        /// <param name="name">Name.</param>
+        /// <param name="value">Value.</param>
+        public WebKeyValue(string key, string value){
+            this.Key = key;
+            this.Value = value;
+        }
+    }
+        
+    public class WebResponseString {
+        [ApiMember(Name="Response", Description = "Response", ParameterType = "query", DataType = "String", IsRequired = true)]
+        public string Response { get; set; }
+
+        public WebResponseString(string response){
+            this.Response = response;
+        }
+    }
+
+    public class WebResponseBool {
+        [ApiMember(Name="Response", Description = "Response", ParameterType = "query", DataType = "bool", IsRequired = true)]
+        public bool Response { get; set; }
+
+        public WebResponseBool(bool response){
+            this.Response = response;
+        }
+    }
+}
+
---
+++ Terradue.WebService.Model/Terradue/WebService/Model/WebDataPackage.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using ServiceStack.ServiceHost;
+using System.Collections.Generic;
+using Terradue.Portal;
+
+namespace Terradue.WebService.Model {
+    [Route("/datapackage/{Id}", "GET", Summary = "GET a datapackage", Notes = "")]
+    public class GetDataPackage : IReturn<WebDataPackage>
+    {
+        [ApiMember(Name="Id", Description = "Data Package id", ParameterType = "query", DataType = "int", IsRequired = true)]
+        public int Id { get; set; }
+    }
+
+    [Route("/datapackage", "GET", Summary = "GET all datapackages", Notes = "")]
+    public class GetAllDataPackages : IReturn<List<WebDataPackage>> {}
+
+    [Route("/datapackage", "POST", Summary = "POST a datapackage", Notes = "Add a new datapackage in database")]
+    public class CreateDataPackage : WebDataPackage, IReturn<WebDataPackage>{}
+
+    [Route("/datapackage", "PUT", Summary = "PUT a datapackage", Notes = "Update a datapackage in database")]
+    public class UpdateDataPackage : WebDataPackage, IReturn<WebDataPackage>{}
+
+    [Route("/datapackage/{Id}", "DELETE", Summary = "DELETE a datapackage", Notes = "Delete a datapackage from database")]
+    public class DeleteDataPackage : IReturn<WebResponseBool>
+    {
+        [ApiMember(Name="Id", Description = "datapackage id", ParameterType = "query", DataType = "int", IsRequired = true)]
+        public int Id { get; set; }
+    }
+        
+    [Route("/datapackage/{DpId}/item", "POST", Summary = "POST item to datapackage", Notes = "datapackage item is contained in the body")]
+    public class AddItemToDataPackage : WebDataPackageItem, IReturn<WebDataPackage>
+    {
+        [ApiMember(Name="DpId", Description = "Data Package Id", ParameterType = "query", DataType = "int", IsRequired = true)]
+        public int DpId { get; set; }
+    }
+
+    [Route("/datapackage/{DpId}/item/{Id}", "DELETE", Summary = "DELETE item from datapackage", Notes = "")]
+    public class RemoveItemFromDataPackage : IReturn<WebDataPackage>
+    {
+        [ApiMember(Name="DpId", Description = "Data Package Id", ParameterType = "query", DataType = "int", IsRequired = true)]
+        public int DpId { get; set; }
+
+        [ApiMember(Name="Id", Description = "datapackage id", ParameterType = "query", DataType = "int", IsRequired = true)]
+        public int Id { get; set; }
+    }
+
+    public class WebDataPackage : WebEntity
+    {
+        [ApiMember(Name="IsDefault", Description = "Remote resource visibility (default = public)", ParameterType = "path", DataType = "bool", IsRequired = true)]
+        public bool IsDefault { get; set; }
+        [ApiMember(Name="Items", Description = "Data Package Resources", ParameterType = "path", DataType = "List<DataPackageItem>", IsRequired = false)]
+        public List<WebDataPackageItem> Items { get; set; }
+
+        public WebDataPackage () {}
+
+        public WebDataPackage (RemoteResourceSet entity) : base(entity){
+            this.IsDefault = entity.IsDefault;
+            this.Items = new List<WebDataPackageItem>();
+            foreach (RemoteResource item in entity.Resources) this.Items.Add(new WebDataPackageItem(item));
+        }
+            
+        /// <summary>
+        /// Transform into the entity.
+        /// </summary>
+        /// <returns>The entity.</returns>
+        /// <param name="context">Context.</param>
+        public RemoteResourceSet ToEntity(IfyContext context, RemoteResourceSet input){
+            RemoteResourceSet result = (input == null ? new RemoteResourceSet(context) : input);
+
+            result.Name = this.Name;
+            result.Identifier = this.Identifier;
+            result.IsDefault = this.IsDefault;
+
+            return result;
+        }
+    }
+
+    public class WebDataPackageItem : WebEntity
+    {
+        [ApiMember(Name="ResourceSetId", Description = "Remote resource set Id", ParameterType = "path", DataType = "int", IsRequired = false)]
+        public int ResourceSetId { get; set; }
+        [ApiMember(Name="Location", Description = "Remote resource Location", ParameterType = "path", DataType = "string", IsRequired = true)]
+        public string Location { get; set; }
+
+        public WebDataPackageItem () {}
+
+        public WebDataPackageItem (RemoteResource entity) : base(entity){
+            this.Location = entity.Location;
+            this.ResourceSetId = entity.ResourceSetId;
+            this.Name = entity.Name;
+        }
+
+        public RemoteResource ToEntity(IfyContext context, RemoteResource input){
+            RemoteResource result = (input == null ? new RemoteResource(context) : input);
+
+            result.Location = this.Location;
+            result.Name = this.Name;
+            return result;
+        }
+
+    }
+}
+
