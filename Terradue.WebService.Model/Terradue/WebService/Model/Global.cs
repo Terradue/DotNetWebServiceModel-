02d266a0a5cd5f69b5cd1ab404af5c83fe66f697
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2015-11-25 16:48:57 +0100

    T2 api
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.WebService.Model/Terradue/WebService/Model/Global.cs
+++ Terradue.WebService.Model/Terradue/WebService/Model/Global.cs
@@ -9,6 +9,30 @@ using System.Collections.Generic;
 
 @}
 
+
+\defgroup T2API T2 API
+@{
+
+    T2 API is an external backend interface of the platform exposed for any users and mainly intented for the web site pages and widgets to interacts with the system.
+
+    It is a REST web service with usual CRUD (create, read, update, delete) functions for the objects managed by the portal:
+
+    - Data Package
+    - Web Feature
+    - Group
+    - Image
+    - News
+    - Series
+    - Service
+    - User
+    - Wps Provider
+
+    It also offers for most of the items an OpenSearch interface to discover or search them efficiently.
+
+    \xrefitem cptype_int "Interfaces" "Interfaces"
+
+@}
+
 */
 
 namespace Terradue.WebService.Model {
