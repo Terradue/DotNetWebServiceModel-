98940fbbfbe832505e30c8d7419320c81e9b0ea1
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-01-15 12:35:08 +0100

    doc
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.WebService.Model/Terradue/WebService/Model/Global.cs
+++ Terradue.WebService.Model/Terradue/WebService/Model/Global.cs
@@ -13,7 +13,7 @@ using System.Collections.Generic;
 \defgroup REST REST handler
 @{
 
-This component enables the full web service stack to intercat with the back-end.
+This component enables the full web service stack to interact with the back-end.
 It allows to control many of the business objects in the system with REST CRUD (Create, Read, Update, Delete) operations
 
 \ingroup WebServices
