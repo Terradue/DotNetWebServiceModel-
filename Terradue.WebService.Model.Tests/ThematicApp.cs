356054f56e75bb9b9e9d83fa3993ba568db118ec
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-03-08 09:18:20 +0100

    initial commit for themeatic app xml

---
+++ Terradue.WebService.Model.Tests/ThematicApp.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using Terradue.OpenSearch.Result;
+using System.IO;
+using System.Xml;
+using Terradue.OpenSearch.GeoJson.Result;
+
+namespace Terradue.WebService.Model.Tests {
+    
+    [TestFixture()]
+    public class ThematicApp {
+        
+        [Test()]
+        public void XmlToJson() {
+
+            FileStream file = new FileStream("../../../Models/thematicapp.xml", FileMode.Open, FileAccess.Read);
+
+            var xr = XmlReader.Create(file,new XmlReaderSettings(){
+                IgnoreWhitespace = true
+            });
+
+            AtomFeed feed = AtomFeed.Load(xr);
+
+            FeatureCollectionResult fc = FeatureCollectionResult.FromOpenSearchResultCollection(feed);
+
+            string json = fc.SerializeToString();
+
+
+
+        }
+    }
+}
+
